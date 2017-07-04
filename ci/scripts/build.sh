e811e3432733b909c6e16ea8d0dba5dac77ee6e8
Phillip Webb <pwebb@pivotal.io>
2017-07-04 09:20:36 -0700

    Hacking;

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -3,4 +3,4 @@
 mkdir -p m2 && ln -s "$(pwd)/m2" ~/.m2
 
 cd artifactory-resource
-./mvnw install -DskipITs
+./mvnw install -Prun-local-artifactory
--- ci/scripts/integration-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir -p m2 && ln -s "$(pwd)/m2" ~/.m2
-
