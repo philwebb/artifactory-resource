65daae31602a28c62a8a5fe99b9cc90b4787a2b7
Madhura Bhave <mbhave@pivotal.io>
2017-07-11 12:02:02 -0700

    Hacking

---
+++ ci/scripts/build-test-tag.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+mkdir -p m2 && ln -s "$(pwd)/m2" ~/.m2
+
+version=$(cat version/version)
+cd artifactory-resource
+#update pom on detached head
+sed -ie 's|<revision>.*</revision>|<revision>$version</revision>|' pom.xml
+
+#build and test
+./mvnw install
+
+git add pom.xml
+git commit -m"Release version $version"
+
+#tag
+git tag v$version
+
+major=$(echo $version | cut -d'.' -f 1)
+minor=$(echo $version | cut -d'.' -f 2)
+patch=$(echo $version | cut -d'.' -f 3)
+#bump
+snapshot_version=$major.$minor.$((patch+1))-SNAPSHOT
+git reset --hard HEAD^1
+.sed -ie 's|<revision>.*</revision>|<revision>$snapshot_version</revision>|' pom.xml
+git add pom.xml
+git commit --message "v$snapshot_version Development"
+
+cp artifactory-resource/target/artifactory-resource-*.jar generated-artifact
+cp -r artifactory-resource artifactory-resource-updated
--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -3,6 +3,9 @@
 mkdir -p m2 && ln -s "$(pwd)/m2" ~/.m2
 
 cd artifactory-resource
+	current=xmllint --xpath '/*[local-name()="project"]/*[local-name()="properties"]/*[local-name()="revision"]/text() pom.xml'
 	./mvnw install -Prun-local-artifactory
 cd ..
+echo $current > snapshot-version/version
+echo "Current version is $current"
 cp artifactory-resource/target/artifactory-resource-*.jar generated-artifact
