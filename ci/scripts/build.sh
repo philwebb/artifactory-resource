f0a11313c3ce49a8d60875622fb4283538120cc4
Phillip Webb <pwebb@pivotal.io>
2017-08-21 12:03:07 -0700

    Hacking

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -10,4 +10,5 @@ pushd git-repo > /dev/null
 run_maven clean install -Prun-local-artifactory
 popd > /dev/null
 
-cp git-repo/target/artifactory-resource.jar built-artifact
+cp git-repo/target/artifactory-resource.jar built-artifact/
+ls -l built-artifact
