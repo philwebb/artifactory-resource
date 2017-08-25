71b3bf7785c8d78237cb6a6b15fe9df0fdd45a1e
Phillip Webb <pwebb@pivotal.io>
2017-08-25 15:10:24 -0700

    Hacking

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-source $(dirname $0)/common.sh
+source /opt/concourse-java.sh
 
 setup_symlinks
 cleanup_maven_repo "io.spring.concourse.artifactoryresource"
 
 pushd git-repo > /dev/null
 version=$(get_revision_from_pom)
-run_maven clean install -Prun-local-artifactory -DskipTests
+run_maven clean install -Prun-local-artifactory
 popd > /dev/null
 
 cp git-repo/target/artifactory-resource.jar built-artifact/
--- ci/scripts/scratch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-# set -x
-source $(dirname $0)/common.sh
-
-version=$(get_relase_version "1.2.3.RELEASE")
-echo $version
-
