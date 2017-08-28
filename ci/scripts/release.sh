c2aed81928dc36885aa00023df4c9478406fbf31
Phillip Webb <pwebb@pivotal.io>
2017-08-28 11:22:18 -0700

    Hacking

--- ci/scripts/release.sh
+++ ci/scripts/release.sh
@@ -6,24 +6,22 @@ source $(dirname $0)/common.sh
 git clone git-repo release-git-repo
 
 pushd release-git-repo > /dev/null
-git branch
 snapshotVersion=$( get_revision_from_pom )
 releaseVersion=$( strip_snapshot_suffix "$snapshotVersion" )
 nextVersion=$( bump_version_number "$snapshotVersion" )
 echo "Releasing $releaseVersion (next SNAPSHOT will be $nextVersion)"
 set_revision_to_pom "$releaseVersion"
-git config user.name "Spring Buildmaster"
-git config user.email "buildmaster@springframework.org"
-git add pom.xml
-git commit -m"Release v$releaseVersion"
-git tag -a "v$releaseVersion" -m"Release version v$releaseVersion"
+git config user.name "Spring Buildmaster" > /dev/null
+git config user.email "buildmaster@springframework.org" > /dev/null
+git add pom.xml > /dev/null
+git commit -m"Release v$releaseVersion" > /dev/null
+git tag -a "v$releaseVersion" -m"Release v$releaseVersion" > /dev/null
 build
 echo "Setting next development version (v$nextVersion)"
-git reset --hard HEAD^
+git reset --hard HEAD^ > /dev/null
 set_revision_to_pom "$nextVersion"
-git add pom.xml
-git commit -m"Next development version (v$releaseVersion)"
-git show HEAD
+git add pom.xml > /dev/null
+git commit -m"Next development version (v$nextVersion)" > /dev/null
 popd > /dev/null
 
 cp release-git-repo/target/artifactory-resource.jar built-artifact/
